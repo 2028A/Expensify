3a011e289fb221859c4129d096e6fed94c18dda6
Lijun Jiang <lijunjiang67@hotmail.com>
2021-02-28 18:58:00 -0600

    remove dummy data

--- src/app.js
+++ src/app.js
@@ -9,15 +9,12 @@ import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-//import React from 'react';
+
 
 var appRoot=document.getElementById("app");
 
 const store=configStore();
 
-store.dispatch(addExpense({description:'Office Rent',amount:14500,createAt:1238}));
-store.dispatch(addExpense({description:'Gas Bill',amount:56780,createAt:899}));
-store.dispatch(addExpense({description:'Water Bill',amount:26780,createAt:1899}));
 const jsx=(
     <Provider store={store}>
         <AppRouter />
