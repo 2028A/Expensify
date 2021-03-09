24b03a88cc9484d879ba02e2f9237c86de7f206b
Lijun Jiang <lijunjiang67@hotmail.com>
2021-03-08 20:56:47 -0600

    update the display format

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,12 +3,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 const  ExpenseListItem= ({id,description,amount,createAt})=>(
     <div>
        <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
         
-        <p>{amount} - {moment(createAt).format('MM-DD-YYYY')} </p>
+        <p>{numeral(amount/100).format('$0,0.00')} 
+        - {moment(createAt).format('MMMM Do, YYYY')} </p>
        
     </div>
 );
--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports=(env)=>{
     //entry:path.resolve(__dirname,'src')+'/app.js',
     entry:'./src/app.js',
     output: {
-        path: path.join(__dirname, 'public'),
+        path: path.join(__dirname, 'public','dist'),
         filename: 'bundle.js',
       },
     module:{
@@ -26,6 +26,7 @@ module.exports=(env)=>{
     devtool: isProduction ? 'source-map': "eval-cheap-module-source-map",
     devServer:{
         contentBase:path.join(__dirname,'public'),
+        publicPath:'/dist/',
         port:8080,
         watchContentBase:true,
         historyApiFallback: true,
